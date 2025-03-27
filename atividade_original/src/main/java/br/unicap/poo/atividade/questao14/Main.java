e6358922954a180f672b5114b188b80eb4451894
Thiago Henrique <79538085+thiagohfmelo@users.noreply.github.com>
2025-03-27 19:28:16 -0300

    Delete atividade_original/src/main/java/br/unicap/poo/atividade/questao14 directory

--- atividade_original/src/main/java/br/unicap/poo/atividade/questao14/Main.java
+++
@@ -1,8 +0,0 @@
-package br.unicap.poo.atividade.questao14;
-
-public class Main {
-    public static void main(String[] args) {
-        Tarefa tarefa1 = new Tarefa("Estudar para a prova", 1);
-        tarefa1.exibirTarefa();
-    }
-}
