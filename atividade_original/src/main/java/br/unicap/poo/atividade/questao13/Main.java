df4dd046e37a01a3896887a83f06685f5fbb5344
Thiago Henrique <79538085+thiagohfmelo@users.noreply.github.com>
2025-03-27 19:28:25 -0300

    Delete atividade_original/src/main/java/br/unicap/poo/atividade/questao13 directory

--- atividade_original/src/main/java/br/unicap/poo/atividade/questao13/Main.java
+++
@@ -1,8 +0,0 @@
-package br.unicap.poo.atividade.questao13;
-
-public class Main {
-    public static void main(String[] args) {
-        Contato contato1 = new Contato("João", "123456789", "joao@email.com");
-        contato1.exibirContato();
-    }
-}
